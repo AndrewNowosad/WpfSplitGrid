1abb56e9b2dfd377aea3f56aac9b9a4c0b75644c
Andrew <nowosad@inbox.ru>
2019-01-23 08:30:19 +0400

    WeekVm class move to WeekVm.cs file

--- WpfSplitGrid/ViewModels/MainVm.cs
+++ WpfSplitGrid/ViewModels/MainVm.cs
@@ -27,16 +27,4 @@ namespace WpfSplitGrid.ViewModels
             }
         }
     }
-
-    class WeekVm : Vm
-    {
-        public int YearNum { get; }
-        public int WeekNum { get; }
-
-        public WeekVm(int yearNum, int weekNum)
-        {
-            YearNum = yearNum;
-            WeekNum = weekNum;
-        }
-    }
 }
---
+++ WpfSplitGrid/ViewModels/WeekVm.cs
@@ -0,0 +1,14 @@
+﻿namespace WpfSplitGrid.ViewModels
+{
+    class WeekVm : Vm
+    {
+        public int YearNum { get; }
+        public int WeekNum { get; }
+
+        public WeekVm(int yearNum, int weekNum)
+        {
+            YearNum = yearNum;
+            WeekNum = weekNum;
+        }
+    }
+}
